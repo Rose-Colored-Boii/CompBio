6315f66511e1c1304e1ce5f998334b53c4033981
Liam Leirs <liam.leirs@student.uantwerpen.be>
2024-05-10 21:50:57 +0200

    Delete PDBParser.py

--- PDBParser.py
+++
@@ -1,9 +0,0 @@
-from Bio.PDB import *
-parser = PDBParser()
-structure = parser.get_structure("1ce6", "1ce6.pdb")
-
-for model in structure:
-    chain = model["B"]
-    for residue in chain:
-        for atom in residue:
-            print(atom.id, atom.get_coord())
